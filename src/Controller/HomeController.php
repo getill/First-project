c217352631d588b92f1bb76064e2373173066688
getill <98155180+getill@users.noreply.github.com>
2023-01-28 22:40:19 +0100

    Twig introduction
    Looks really powerfull !

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,14 +1,18 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/')]
     public function  number()
     {
         $number = rand(0, 100);
 
-        dd($number);
+        return $this->render('/base.html.twig', [
+            "number" => $number
+        ]);
+
     }
 }
